726a856f78168b2963c082fac5864d1cf78f28ba
idan <idandrd@gmail.com>
2020-04-15 13:58:30 +0300

    added easter egg

---
+++ www/js/easter-egg.js
@@ -0,0 +1,24 @@
+(() => {
+  const imageUrl = "img/ducks.jpg";
+  let easterEggCounter = 0;
+  const header = document.getElementsByClassName("masthead")[0];
+  header.onclick = onEasterClick;
+
+  function onEasterClick() {
+    if (++easterEggCounter == 3) {
+      easterEgg();
+    }
+    preLoadImage();
+  }
+
+  function preLoadImage() {
+    if (easterEggCounter == 2) {
+      const img = new Image();
+      img.src = imageUrl;
+    }
+  }
+
+  function easterEgg() {
+    header.style.backgroundImage = `url('${imageUrl}')`;
+  }
+})();
